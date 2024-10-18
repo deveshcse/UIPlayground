c285299b9ef6b0273591ca5e318575107bb06535
deveshcse <d.mishra56603@gmail.com>
2024-10-19 01:18:50 +0530

    project setup done

--- my-corausal/src/App.jsx
+++ my-corausal/src/App.jsx
@@ -1,17 +1,40 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-
+import { useState, useEffect } from "react";
 function App() {
-  const [count, setCount] = useState(0);
+  const [images, setImages] = useState([]);
+
+const fetchImages = async () => {
+  const url = 'https://www.reddit.com/r/aww/top/.json?t=all';
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status}`);
+    }
+    const result = await response.json();
+    const data = result?.data?.children ?? [];
+    const list = data.filter(
+      (item) => item.data.url_overridden_by_dest?.includes('.jpg')
+    );
+    const imageList = list.map((item) => item.data.url_overridden_by_dest);
+    setImages(imageList);
+  } catch (error) {
+    console.error('Error fetching images:', error);
+  }
+};
+
+
+  useEffect(()=>{
+    fetchImages();
+    console.log(images);
+    
+  }, []);
 
   return (
-    <div className="flex items-center justify-center" >
+    <div className="h-screen flex items-center justify-center gap-10 bg-slate-600" >
       
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <button className="bg-red-600 border-2 border-black rounded-lg text-3xl p-2 font-bold">Left</button>
+      <img src="" alt="image is not available" />
+      <button className="bg-red-600 border-2 border-black rounded-lg text-3xl p-2 font-bold">Right</button>
     </div>
   );
 }
